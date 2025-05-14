4a950d80f29f93b7b589439f53aaba7c4e17f334
HarshadaP2305 <harshadapatil1068@gmail.com>
2025-05-14 22:06:45 +0530

    Add files via upload
    Changes

--- script.js
+++ script.js
@@ -49,6 +49,10 @@ mobileInput.addEventListener('input', function () {
     }
 });
 
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+}
 
 // Toggle the navigation menu on mobile
 document.addEventListener('DOMContentLoaded', () => {
